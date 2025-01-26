ae11e0e44c439ea76ab66857af135b17001cfe01
danielarodval <49764316+danielarodval@users.noreply.github.com>
2025-01-26 14:22:26 -0500

    added env, requirements, start, and updates
    added in python venv
    added requirements.txt to init using start.bat calling streamlit and install required dependencies
    updated rental_agreement to use ds_config and added alternative links to run added in sidebar and fixed errors with session_state

---
+++ get_requirements.py
@@ -0,0 +1,6 @@
+# import pipreqs
+
+# pipreqs_path = 'requirements.txt'
+# pipreqs.generate_requirements_file('.', pipreqs_path)
+
+print("test")
--- rental_agreement_agent.py
+++ rental_agreement_agent.py
@@ -4,7 +4,7 @@ import base64
 from docusign_esign import Document, ApiClient, EnvelopeDefinition, EnvelopesApi, Signer, Tabs, SignHere, Recipients
 import requests
 from streamlit_oauth import OAuth2Component
-import ds_config
+from ds_config import DS_CONFIG as ds_config
 
 # load environment variables
 from dotenv import load_dotenv
@@ -22,45 +22,56 @@ def get_user(access_token):
     return response
 
 #%% streamlit app
-
 st.title('Rental Agreement Agent')
 st.write("This is a simple web app that assists you in understanding the terms of a rental agreement. It uses a pre-trained model to extract the key terms from the agreement and provides a summary of the agreement. You can also ask questions about the agreement and get answers based on the extracted terms.")
 st.divider()
 
 #%% configuration
-
-AUTHORIZATION_URL = f"{ds_config.AUTHORIZATION_SERVER}/oauth/auth"
-TOKEN_URL = f"{ds_config.AUTHORIZATION_SERVER}/oauth/token"
-REDIRECT_URI = ds_config.APP_URL_TS_MOBILE
-CLIENT_ID = ds_config.DS_CLIENT_ID
-CLIENT_SECRET = ds_config.DS_CLIENT_SECRET
-REDIRECT_URI = ds_config.APP_URL_TS_MOBILE
+AUTHORIZATION_URL = f"{ds_config['authorization_server']}/oauth/auth"
+TOKEN_URL = f"{ds_config['authorization_server']}/oauth/token"
+REDIRECT_URI = ds_config['app_url_lcl']
+CLIENT_ID = ds_config['ds_client_id']
+CLIENT_SECRET = ds_config['ds_client_secret']
+REDIRECT_URI = ds_config['app_url_lcl']
 #BTN_URL = f"https://account-d.docusign.com/oauth/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=signature&state=d8LjbTmbZVlirw77oqw3OReWk5v9jC&code_challenge=RMK30ueseHEu6V7FImJA0KFJI1JCVzjc-Pgbo_adPUA&code_challenge_method=S256&approval_prompt=auto"
 SCOPES = "signature adm_store_unified_repo_read"
 
 oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZATION_URL, TOKEN_URL)
 
-#st.write("Access Token: ", st.session_state.get("token"))
-#st.write("User: ", st.session_state.get("user"))
-st.write("Access Token: ",'access_token' in st.session_state.get("token"))
-st.write("User: ", 'name' in st.session_state.get("user"))
+with st.sidebar: # check access token and user
+    #st.write("Access Token: ", st.session_state.get("token"))
+    #st.write("User: ", st.session_state.get("user"))
+    try:
+        st.write("Access Token: ", 'access_token' in st.session_state.get("token"))
+    except:
+        st.write("Access Token: ", False)
 
-if 'access_token' not in st.session_state.get("token"):
+    try:
+        st.write("User: ", 'name' in st.session_state.get("user"))
+    except:
+        st.write("User: ", False)
+
+if 'access_token' not in st.session_state:
     result = oauth2.authorize_button("Continue with Docusign", REDIRECT_URI, SCOPES)
-    st.write("result: ", result)
-    if result:
-        st.session_state.token = result.get("token")
-        user = get_user(st.session_state.get("token").get("access_token"))
-        st.session_state.user = user
-        st.rerun()
+    with st.spinner("Waiting for authorization..."):
+        if result:
+            st.session_state.token = result.get("token")
+            user = get_user(st.session_state.get("token").get("access_token"))
+            st.session_state.user = user
+            st.rerun()
 
-access_token = st.session_state.get("token").get("access_token")
-account_id = st.session_state.get("user").get("accounts")[0].get("account_id")
-base_path = "https://demo.docusign.net/restapi"
+if st.session_state.get("token") and st.session_state.get("user"):
+    access_token = st.session_state.get("token").get("access_token")
+    account_id = st.session_state.get("user").get("accounts")[0].get("account_id")
+    base_path = "https://demo.docusign.net/restapi"
 
-api_client = ApiClient()
-api_client.host = base_path
-api_client.set_default_header("Authorization", f"Bearer {access_token}")
+    api_client = ApiClient()
+    api_client.host = base_path
+    api_client.set_default_header("Authorization", f"Bearer {access_token}")
+
+else:
+    st.write("Please log in to continue.")
+    access_token = None
 
 #%% streamlit file uploader
 with st.expander("Upload Rental Agreement"):
